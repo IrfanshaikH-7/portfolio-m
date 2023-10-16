33e795c284f870635c8f7ffba8f6dac1bcb2f101
Irfan shaik <414irfann@gmail.com>
2023-10-16 23:02:34 +0530

    29.projects rendering

--- components/portpolio/more-project.tsx
+++ components/portpolio/more-project.tsx
@@ -1,10 +1,17 @@
-
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import ProjectCard from "./project-card";
-import { MoveDown, Triangle } from "lucide-react";
+import { Triangle } from "lucide-react";
+import { db } from "@/lib/db";
 
 
-const MoreProjects = () => {
+const MoreProjects = async() => {
+
+    const fullstack = await db.project.findMany( {where: {category: 'fullstack' } } );
+    const frontend = await db.project.findMany( {where: {category: 'frontend' } } );
+    const backend = await db.project.findMany( {where: {category: 'backend' } } );
+    const others = await db.project.findMany( {where: {category: 'others ' } } );
+
+    
     return (
         <div className="flex flex-col lg:flex-row gap-2  justify-start lg:justify-center items-center h-full w-full p-4 ">
             <div className="md:w-full lg:w-20 lg:h-full p-2">
@@ -23,7 +30,7 @@ const MoreProjects = () => {
                                 <h3 className="text-3xl text-center justify-center font-syne uppercase ">Fullstack</h3>
                                 <Triangle className="rotate-180 fill-black dark:fill-white h-4 w-4" />
                             </div>
-                            <ProjectCard />
+                            <ProjectCard cardDetails={fullstack}/>
                         </section>
                     </TabsContent>
                     <TabsContent value="frontend">
@@ -32,16 +39,17 @@ const MoreProjects = () => {
                                 <h3 className="text-3xl text-center justify-center font-syne uppercase ">frontend</h3>
                                 <Triangle className="rotate-180 fill-black dark:fill-white h-4 w-4" />
                             </div>
-                            <ProjectCard />
+                            <ProjectCard cardDetails={frontend}/>
                         </section>
                     </TabsContent>
                     <TabsContent value="python">
                         <section className="h-full w-full  overflow-y-scroll no-scrollbar">
                             <div className="flex flex-col items-center justify-center mb-12 py-2">
-                                <h3 className="text-3xl text-center justify-center font-syne uppercase ">python</h3>
+                                <h3 className="text-3xl text-center justif
+                                center font-syne uppercase ">python</h3>
                                 <Triangle className="rotate-180 fill-black dark:fill-white h-4 w-4" />
                             </div>
-                            <ProjectCard />
+                            <ProjectCard cardDetails={backend}/>
                         </section>
                     </TabsContent>
                     <TabsContent value="others">
@@ -51,7 +59,7 @@ const MoreProjects = () => {
                                 <Triangle className="rotate-180 fill-black dark:fill-white h-4 w-4" />
                             </div>
 
-                            <ProjectCard />
+                            <ProjectCard cardDetails={others}/>
                         </section>
                     </TabsContent>
                     <TabsList className="flex md:flex-col xl:w-2/5 mt-2 md:mt-0 md:h-full md:px-12">
--- components/portpolio/project-card.tsx
+++ components/portpolio/project-card.tsx
@@ -1,45 +1,65 @@
+import { Project } from "@prisma/client";
+import Image from "next/image";
 
-const ProjectCard = () => {
-    return ( 
-        <>
-        <section className="grid sm:grid-cols-2 h-full gap-3 py-8 px-4">
-
-        
-        <div className=" flex items-center  justify-center rounded-lg shadow-md h-40 md:h-60  xl:h-68 w-full bg-slate-900 border border-slate-300">
-            <div className=" bg-gray-900 rounded-lg">
-hhh
-            </div>
-        </div>
-        
-        <div className=" flex items-center justify-center rounded-lg shadow-md h-40 md:h-60  xl:h-68 w-full bg-slate-900 border border-slate-300">
-            <div className=" bg-gray-900 rounded-lg">
-vouuof      
-            </div>
-        </div>
-        <div className=" flex items-center justify-center rounded-lg shadow-md h-40 md:h-60  xl:h-68 w-full bg-slate-900 border border-slate-300">
-            <div className=" bg-gray-900 rounded-lg">
-uificv
-            </div>
-        </div>
-        <div className=" flex items-center justify-center rounded-lg shadow-md h-40 md:h-60  xl:h-68 w-full bg-slate-900 border border-slate-300">
-            <div className=" bg-gray-900 rounded-lg">
-uificv
-            </div>
-        </div>
-        <div className=" flex items-center justify-center rounded-lg shadow-md h-40 md:h-60  xl:h-68 w-full bg-slate-900 border border-slate-300">
-            <div className=" bg-gray-900 rounded-lg">
-uificv
-            </div>
-        </div>
-        <div className=" flex items-center justify-center rounded-lg shadow-md h-40 md:h-60  xl:h-68 w-full bg-slate-900 border border-slate-300">
-            <div className=" bg-gray-900 rounded-lg">
-uificv
-            </div>
-        </div>
-        </section>
-        </>
-        
-     );
+type Props = {
+    cardDetails: Project[]
 }
- 
+const ProjectCard = ({ cardDetails }: Props) => {
+    if (!cardDetails || cardDetails.length < 1) {
+        return (
+            <div className="h-full w-full flex items-center justify-center">
+                <p className="text-slate-400 font-semibold text-2xl">No projects yet...</p>
+            </div>
+        )
+    }
+    return (
+        <>
+            <section className="grid sm:grid-cols-2 h-full gap-3 py-8 px-4">
+
+
+
+
+                {
+                    // h-40 md:h-60  xl:h-68 
+                    cardDetails.map((item) => (
+                        <div key={item.id} className=" flex items-center  justify-center rounded-lg shadow-md w-full bg-slate-900 border border-slate-300  md:aspect-square relative">
+                            <div className=" bg-gray-900 rounded-lg h-full w-full px-2">
+
+                                <div className="relative h-full w-full space-y-1 md:space-y-3 py-2">
+                                    <div className="aspect-video w-full relative">
+                                        <Image
+                                            src={item.imgUrl[0]}
+                                            alt="projectImg"
+                                            fill
+                                            className=" z-10 py-2 rounded-2xl"
+                                        />
+                                        <div className="h-12 w-40 bg-white absolute rounded-xl bottom-1 right-1" />
+                                    </div>
+                                    <div className="flex gap-2 sm:gap-1 xl:gap-1 bg-white rounded-3xl px-2 py-1 sm:py-[2px] xl:py-1 ">
+                                        {
+                                            item.tags.map((tag) => (
+                                                <p className="text-sm sm:text-[9px] xl:text-[11px] text-slate-600 ">#{tag}</p>
+
+                                            ))
+                                        }
+                                    </div>
+                                    <div >
+                                        <p className="px-4 text-slate-300 font-semibold text-center"><span className="font-semibold ">⪧{" "}</span>{item.note}</p>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    ))
+                }
+
+
+
+            </section>
+        </>
+
+    );
+}
+
 export default ProjectCard;
